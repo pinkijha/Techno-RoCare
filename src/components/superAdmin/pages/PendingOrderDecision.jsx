e1c6ec02534c079f9dc5d7ccf6939b45c986b203
Pinki Jha <pinkijha1310@gmail.com>
2025-02-20 15:05:08 +0530

    add pending order preview  page

--- src/components/superAdmin/pages/PendingOrderDecision.jsx
+++ src/components/superAdmin/pages/PendingOrderDecision.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import { IoMdArrowRoundBack } from "react-icons/io";
 
-const PendingOrderDecision = () => {
+const PendingOrderDecision = ({ order, onBack }) => {
   return (
     <div className="h-screen bg-white p-6 rounded-xl text-[#263138] shadow-sm">
-    
+    <button
+            onClick={onBack}
+            className=" text-2xl outline-none cursor-pointer rounded"
+          >
+            <IoMdArrowRoundBack />
+          </button>
 
-    <label className="block font-semibold mb-1">Select Status</label>
+    <label className="md:mt-4 block font-semibold mb-1">Select Status</label>
     <select className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-300 mb-5">
       <option>Select a status</option>
       <option>Accept</option>
--- src/components/superAdmin/pages/PendingOrderPrev.jsx
+++ src/components/superAdmin/pages/PendingOrderPrev.jsx
@@ -1,18 +1,65 @@
-const PendingOrderPrev = ({ order, onBack }) => {
+import { IoMdArrowRoundBack, IoMdTimer } from "react-icons/io";
+import { FaRegCalendarAlt } from "react-icons/fa";
+import { TbDeviceMobileDollar } from "react-icons/tb";
+import { PiShapes } from "react-icons/pi";
+
+const PendingOrderPrev = ({ onBack }) => {
   return (
-    <div className="p-4 bg-white shadow-lg rounded-md">
-      <h2 className="text-xl font-bold mb-4">Preview Order</h2>
-      <p><strong>Order ID:</strong> {order?.orderId}</p>
-      <p><strong>Order Date:</strong> {order?.orderDate}</p>
-      <p><strong>Service Partner:</strong> {order?.partnerName}</p>
-      
-      <button
+    <div className="p-6 bg-white shadow-lg rounded-md">
+       <button
         onClick={onBack}
-        className="mt-4 px-4 py-2 bg-red-500 text-white rounded"
+        className=" text-2xl outline-none cursor-pointer rounded"
       >
-        Back
+        <IoMdArrowRoundBack />
       </button>
+
+        <div className="flex md:mt-4 justify-between border-b pb-4">
+          <p><strong>Order No:</strong> 6019</p>
+          <p><strong>Shipped date:</strong> 01-02-2026</p>
+          <p><strong>Customer Name:</strong> Kartik Ray</p>
+          <p><strong>Mobile Number:</strong> 89xxxxxxxx</p>
+        </div>
+
+        <div className="mt-4">
+          <h2 className="font-semibold text-lg">Order Items</h2>
+          <ul className="list-decimal pl-6 mt-2">
+            <li>
+              <p><strong>Water Purifier Full Checkup</strong></p>
+              <p className="text-gray-600">Problem: Low Water Flow</p>
+              <p className="text-gray-600">Qty: 1</p>
+              <p className="text-gray-600">Price: ₹299</p>
+              <p className="text-gray-600">Service time: 1hr 30min</p>
+            </li>
+          </ul>
+        </div>
+
+        <h2 className="font-semibold mt-4  text-lg">Date And Time Slot Details</h2>
+        <div className="mt-4 w-full flex justify-between ">
+          <p className="flex gap-2 items-center"><FaRegCalendarAlt /> Date: Sun 02</p>
+          <p className="flex gap-2 items-center"><IoMdTimer /> Time: 09:00 am</p>
+          <p className="flex gap-2 items-center"><TbDeviceMobileDollar /> Payment Method: Cash On Delivery</p>
+          <p className="flex gap-2 items-center"><PiShapes /> Category: Water Purifier Repair</p>
+        </div>
+
+        <div className="mt-6">
+          <h2 className="font-semibold text-lg">Payment Summary</h2>
+          <p className="flex justify-between">Item’s total: <span>₹200</span></p>
+          <p className="flex justify-between border-b border-[#ABABAB] pb-2">Taxes and Fee: <span>₹68</span></p>
+          <p className="flex justify-between font-bold pt-2">Total: <span className="font-normal">₹268</span></p>
+        </div>
+
+        <h2 className="font-semibold text-lg mt-6">Shipping And Billing Details</h2>
+        <div className="flex mt-2">
+          <p>Shipping Address: Xxxxxxxx</p>
+          <p className="md:ml-[200px]">Order Status: Pending</p>
+        </div>
+
+        <div className="mt-6 text-center">
+          <button className="bg-[#67D6B9] hover:bg-[#25803D] md:mt-6 cursor-pointer text-white px-6 py-2 rounded-md">Print Order Preview</button>
+        </div>
+     
     </div>
+   
   );
 };
 export default PendingOrderPrev;
